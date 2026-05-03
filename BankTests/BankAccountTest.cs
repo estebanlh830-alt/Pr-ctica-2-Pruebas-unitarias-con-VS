f5618743cf81c5dddeaabef7bed5f2f0d3f955e4
estebanlh830-alt <estebanlh830@gmail.com>
2026-05-03 13:22:27 +0200

    Add files via upload

---
+++ BankTests/BankAccountTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using BankAccountNS;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestMethod]
+        public void Debit_WithValidAmount_UpdatesBalance()
+        {
+            // preparación del caso de prueba
+
+            double beginningBalance =
+            11.99; double debitAmount =
+            4.55; double expected = 7.44;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            // acción a probar
+            account.Debit(debitAmount);
+            // afirmación de la prueba (valor esperado Vs. Valor obtenido)
+            double actual = account.Balance;
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        [ExpectedException(typeof(Exception))]
+        public void Debit_WhenBankAccountIsFrezee_ShouldThrowException()
+        {
+            // preparación del caso de prueba
+            double beginningBalance = 11.99;
+            double debitAmount = -100.00;
+            BankAccount account = new BankAccount("Mr. Bryan Walton", beginningBalance);
+            account.FreezeAccount();
+            // acción a probar
+            account.Debit(debitAmount);
+            // la afirmación es manejado por el atributo ExpectedException
+
+        }
+    }
+}
---
+++ BankTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BankTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BankTests")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1a154225-b9eb-462f-a521-588e4b9d746a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
